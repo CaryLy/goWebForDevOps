d5484ebd92b95856927c6ee6b672270679ff9e72
liqinggai <liqinggai@zuoyebang.com>
2018-06-17 12:29:55 +0800

    2

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 kill -9 $(pgrep webserver)
-cd /data/web/gowork/src/DevOpsAndCloud
+    cd /data/gowork/goWebForDevOps/
 git pull https://github.com/CaryLy/goWebForDevOps.git
 cd webserver/
 ./webserver &
